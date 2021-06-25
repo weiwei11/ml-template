a28cc49175be8d31c483390b03396b561ca13881
ww <weiwei1136611846@163.com>
2021-06-25 10:33:36 +0800

    feature(template.yaml): add auto detect debug mode
    1. add auto detect debug mode
    2. change the fullname format

--- lib/config/resolver.py
+++ lib/config/resolver.py
@@ -1,4 +1,5 @@
 # Author: weiwei
+import sys
 import time
 from datetime import datetime
 from lib.utils.wind import git_info as wgit_info
@@ -12,6 +13,7 @@ def datetime_now(fmt):
 OmegaConf.register_new_resolver('datetime_now', datetime_now, replace=True)
 OmegaConf.register_new_resolver('cur_version', wgit_info.get_hash, replace=True)
 OmegaConf.register_new_resolver('condition', lambda c, x1, x2: x1 if c else x2)
+OmegaConf.register_new_resolver('is_debug', lambda: True if sys.gettrace() else False)
 
 
 if __name__ == '__main__':
